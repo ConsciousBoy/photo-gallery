42927a1a64d1f978def058886c667a11cb1c0bcb
Yashar-AB <yasharabbasiehnl@gmail.com>
2025-05-11 16:28:12 +0200

    Add config.php to repository

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gallery_db');
+define('DB_USER', 'bit_academy');
+define('DB_PASS', 'bit_academy');
+
+
+
+?> 
