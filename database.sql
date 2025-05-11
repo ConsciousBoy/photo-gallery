781f649392ac06537f10874b7025626aa1320b90
Yashar-AB <yasharabbasiehnl@gmail.com>
2025-05-11 17:36:31 +0200

    Update after author rewrite

--- database.sql
+++ database.sql
@@ -42,4 +42,17 @@ INSERT INTO photos (file_path, title) VALUES
 ('sunnydandelionhead.jpg', 'Sunny Dandelion Head'),
 ('unfadingbloom.png', 'Unfading Bloom'),
 ('violetbloom.jpg', 'Violet Bloom'),
-('whiteandyellowdaisy.jpg', 'White and Yellow Daisy');
+('whiteandyellowdaisy.jpg', 'White and Yellow Daisy'),
+('pic1.jpg', 'pic1'),
+('pic2.jpg', 'pic2'),
+('pic3.jpg', 'pic3'),
+('pic4.jpg', 'pic4'),
+('pic5.jpg', 'pic5'),
+('pic6.jpg', 'pic6'),
+('pic7.jpg', 'pic7'),
+('pic8.jpg', 'pic8'),
+('pic9.jpg', 'pic9'),
+('pic10.jpg', 'pic10'),
+('pic11.jpg', 'pic11'),
+('pic12.jpg', 'pic12'),
+('pic13.jpg', 'pic13');
--- update_photos.sql
+++ update_photos.sql
@@ -6,18 +6,7 @@ USE gallery_db;
 
 -- Add your new photos here using this format:
 INSERT INTO photos (file_path, title) VALUES
-('pic1.jpg', 'pic1'),
-('pic2.jpg', 'pic2'),
-('pic3.jpg', 'pic3'),
-('pic4.jpg', 'pic4'),
-('pic5.jpg', 'pic5'),
-('pic6.jpg', 'pic6'),
-('pic7.jpg', 'pic7'),
-('pic8.jpg', 'pic8'),
-('pic9.jpg', 'pic9'),
-('pic10.jpg', 'pic10'),
-('pic11.jpg', 'pic11'),
-('pic12.jpg', 'pic12'),
-('pic13.jpg', 'pic13');
+('*.jpg', '*'),
+;
 
 -- Note: Make sure the file_path matches the actual image files in your uploads directory 
